4216ca05d2d6f5ef32cddf325213b567e01a8928
Anton Perez <anton.sungho.perez@gmail.com>
2021-02-05 05:57:21 +0000

    finished calc distance and calc shortest path

--- src/graph.py
+++ src/graph.py
@@ -2,6 +2,8 @@ class Node:
   def __init__(self, index):
     self.index = index
     self.neighbors = []
+    self.previous = None
+    self.distance = None
 
 class Graph:
   def __init__(self, edges):
@@ -37,6 +39,7 @@ class Graph:
       
 
   def get_nodes_breadth_first(self, initial_node):
+    self.build_from_edges()
     queue = [self.nodes[initial_node]]
     visited = []
     while queue != []:
@@ -50,6 +53,7 @@ class Graph:
     return visited
 
   def get_nodes_depth_first(self, initial_node):
+    self.build_from_edges()
     stack = [self.nodes[initial_node]]
     visited = []
     while stack != []:
@@ -61,3 +65,38 @@ class Graph:
                for neighbor in neighbors 
                if neighbor not in stack and neighbor not in visited] + stack
     return visited
+
+  def set_breadth_first_distance_and_previous(self, starting_node_index): 
+    self.build_from_edges()
+    self.nodes[starting_node_index].distance = 0
+    queue = [self.nodes[starting_node_index]]
+    visited = []
+    while queue != []:
+      visiting = queue[0]
+      current_dist = visiting.distance
+      queue = queue[1:]
+      visited.append(visiting)
+      neighbors = visiting.neighbors
+      for neighbor in neighbors:
+        if neighbor not in queue and neighbor not in visited:
+          neighbor.distance = current_dist + 1
+          neighbor.previous = visiting
+
+      queue = queue + [neighbor 
+                       for neighbor in neighbors 
+                       if neighbor not in queue and neighbor not in visited]
+    
+  def calc_distance(self, starting_node_index, ending_node_index):
+    self.set_breadth_first_distance_and_previous(starting_node_index)
+    return self.nodes[ending_node_index].distance
+
+  def calc_shortest_path(self, starting_node_index, ending_node_index):
+    self.set_breadth_first_distance_and_previous(starting_node_index)
+    
+    current_node = self.nodes[ending_node_index]
+    path_list = [ending_node_index]
+    while current_node.index != starting_node_index:
+      current_node = current_node.previous
+      path_list.append(current_node.index)
+    return path_list[::-1]
+
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -4,7 +4,7 @@ from graph import Graph
 
 edges = [(0,1),(1,2),(1,3),(3,4),(1,4),(4,5)]
 graph = Graph(edges)
-graph.build_from_edges()
+
 
 bf = graph.get_nodes_breadth_first(2)
 print('Testing method get_nodes_breadth_first...')
@@ -14,4 +14,24 @@ print('PASSED'), [node.index for node in bf]
 df = graph.get_nodes_depth_first(2)
 print('Testing method get_nodes_depth_first...')
 assert [node.index for node in df] == [2, 1, 0, 3, 4, 5]
+print('PASSED')
+
+
+edges = [(0,1),(1,2),(1,3),(3,4),(1,4),(4,5)]
+graph = Graph(edges)
+
+print('Testing method calc_distance...')
+assert graph.calc_distance(0,4) == 2
+assert graph.calc_distance(5,2) == 3
+assert graph.calc_distance(0,5) == 3
+assert graph.calc_distance(4,1) == 1
+assert graph.calc_distance(3,3) == 0
+print('PASSED')
+
+print('Testing method calc_shortest_path...')
+assert graph.calc_shortest_path(0,4) == [0, 1, 4], graph.calc_shortest_path(0,4)
+assert graph.calc_shortest_path(5,2) == [5, 4, 1, 2]
+assert graph.calc_shortest_path(0,5) == [0, 1, 4, 5]
+assert graph.calc_shortest_path(4,1) == [4, 1]
+assert graph.calc_shortest_path(3,3) == [3]
 print('PASSED')
